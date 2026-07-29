3b2b187a93d25ba925d38d9f60343996d1680d02
Josepito Quizada <quizadajosepito@gmail.com>
2026-07-29 10:04:42 +0800

    AnimalUpdate

--- database.js
+++ database.js
@@ -1,16 +1,20 @@
+require("dotenv").config();
 const mysql = require("mysql2/promise");
-const fs = require("fs");
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
 
-  ssl: {
-    ca: fs.readFileSync("./certs/isrgrootx1.pem")
-  }
+    waitForConnections: true,
+    connectionLimit: 10,
+
+    ssl: {
+        minVersion: "TLSv1.2",
+        rejectUnauthorized: false
+    }
 });
 
 module.exports = pool;
